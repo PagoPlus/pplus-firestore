5da9098780acd6688b9d1a9b434a69f943472c6d
João Vitor Martins Araújo <jvmartyns2015@gmail.com>
2025-01-15 11:46:03 -0300

    refactor: PPlusFireStore to support configurable token_fetcher and optimizes credential extraction

--- lib/pplus_firestore.ex
+++ lib/pplus_firestore.ex
@@ -2,13 +2,9 @@ defmodule PPlusFireStore do
   @moduledoc """
   Documentation for `PPlusFireStore`.
   """
-
   use Supervisor
 
-  # Note:
-  # Which is better?
-  # - Receive a single module. Then we will have one process for each supervisor (PPlusFireStore)
-  # - Receive a list of modules. Then we will have a single supervisor managing multiple processes.
+  @default_token_fetcher Goth
 
   def start_link(modules) when is_list(modules) do
     Supervisor.start_link(__MODULE__, modules, name: __MODULE__)
@@ -18,13 +14,29 @@ defmodule PPlusFireStore do
   def init(modules) do
     children =
       Enum.map(modules, fn module ->
-        {Goth, name: module, source: {:service_account, credentials(module)}}
+        {token_fetcher(module), name: module, source: {:service_account, credentials(module)}}
       end)
 
     Supervisor.init(children, strategy: :one_for_one)
   end
 
   defp credentials(module) do
-    Map.new(module.config(), fn {k, v} -> {to_string(k), v} end)
+    module.config()
+    |> Keyword.take([
+      :project_id,
+      :private_key_id,
+      :private_key,
+      :client_email,
+      :client_id,
+      :auth_uri,
+      :token_uri,
+      :auth_provider_x509_cert_url,
+      :client_x509_cert_url
+    ])
+    |> Map.new(fn {k, v} -> {to_string(k), v} end)
+  end
+
+  defp token_fetcher(module) do
+    module.config()[:token_fetcher] || @default_token_fetcher
   end
 end
