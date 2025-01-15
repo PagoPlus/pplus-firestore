d20c27119f221153c961c4afcc2ba4404324954a
João Vitor Martins Araújo <jvmartyns2015@gmail.com>
2025-01-15 18:12:15 -0300

    feat: adds support for encoding geographic values ​​with latitude and longitude

--- lib/pplus_firestore/encoder.ex
+++ lib/pplus_firestore/encoder.ex
@@ -7,12 +7,17 @@ defmodule PPlusFireStore.Encoder do
   def encode_value(nil), do: %{nullValue: nil}
   def encode_value({:bytes, value}), do: %{bytesValue: value}
   def encode_value({:ref, value}), do: %{referenceValue: value}
+  def encode_value({:geo, {lat, lng}}), do: %{geoPointValue: %{latitude: lat, longitude: lng}}
   def encode_value(value) when is_boolean(value), do: %{booleanValue: value}
   def encode_value(value) when is_float(value), do: %{doubleValue: value}
   def encode_value(value) when is_integer(value), do: %{integerValue: value}
   def encode_value(value) when is_binary(value), do: %{stringValue: value}
 
   def encode_value({lat, lng}) when is_number(lat) and is_number(lng) do
+    %{geoPointValue: %{latitude: lat, longitude: lng}}
+  end
+
+  def encode_value(%{latitude: lat, longitude: lng}) when is_number(lat) and is_number(lng) do
     %{geoPointValue: %{latitude: lat, longitude: lng}}
   end
 
