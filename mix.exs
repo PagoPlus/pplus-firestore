d28ec38f950460dc4afc3e120164203aad48240f
João Vitor Martins Araújo <87587551+JVMartyns@users.noreply.github.com>
2025-03-06 15:19:47 -0300

    fix: replace "maintaners" to "maintainers"
    Co-authored-by: Fernando Mumbach <fermuch@cryptolab.net>

--- mix.exs
+++ mix.exs
@@ -72,7 +72,7 @@ defmodule PPlusFireStore.MixProject do
     %{
       licenses: ["MIT"],
       links: %{"GitHub" => @source_url},
-      maintaners: ["JVMartyns", "Fernando Mumbach"]
+      maintainers: ["JVMartyns", "Fernando Mumbach"]
     }
   end
 
