7b9a82bc41518fe9d6979b2e2c94913afebec3d9
João Vitor Martins Araújo <jvmartyns2015@gmail.com>
2025-05-16 15:16:41 -0300

    core: update package version to 0.1.6 in mix.exs and README.md

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule PPlusFireStore.MixProject do
   use Mix.Project
 
   @source_url "https://github.com/PagoPlus/pplus-firestore"
-  @version "0.1.5"
+  @version "0.1.6"
 
   def project do
     [
